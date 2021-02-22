43c1a245ae9025220cda7a00400a0c9f36bf3ea3
Samantha Becker <Sam@MacBook-Pro.local>
2021-02-22 18:49:06 -0500

    App.js edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import CourseManager from './components/course-manager';
+import CourseEditor from './components/course-editor/course-editor';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <BrowserRouter>
+        <Route path='/' exact={true}>
+          <Home/>
+        </Route>
+        <Route path='/courses'>
+          <CourseManager/>
+        </Route>
+        <Route path='/editor' exact={true} render={(props) =>
+        <CourseEditor {...props}/>}/>
+      </BrowserRouter>
     </div>
   );
 }
