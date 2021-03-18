165a2337c6f1414a73be39216abc81a372d46c2c
Samantha Becker <Sam@MacBook-Pro.local>
2021-03-17 21:42:17 -0400

    widget reducer implemented

---
+++ src/reducers/widget-reducer.js
@@ -0,0 +1,47 @@
+const initialState = {
+    widgets: []
+}
+
+const widgetReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case 'CREATE_WIDGET':
+          return {
+            ...state,
+            widgets: [
+                ...state.widgets,
+                action.widget
+                ]
+          }
+    case 'DELETE_WIDGET':
+          return {
+            ...state,
+            widgets: state.widgets.filter(widget =>
+                widget.id !== action.widgetToDelete.id)
+          }
+    case 'UPDATE_WIDGET':
+          return {
+            ...state,
+            widgets: state.widgets.map(widget => {
+              if(widget._id === action.widgetToUpdate._id) {
+                return action.widgetToUpdate
+              } else {
+                return widget
+              }
+          }
+    case 'FIND_ALL_WIDGETS_FOR_TOPIC':
+          return {
+            ...state,
+            widgets: action.widgets
+          }
+    case 'FIND_ALL_WIDGETS':
+          return {
+            ...state
+          }
+    case 'FIND_WIDGET':
+          return state
+    default:
+          return state
+  }
+}
+
+export default widgetReducer
