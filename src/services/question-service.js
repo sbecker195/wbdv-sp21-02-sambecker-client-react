8a7e5a596c2c70bec14cb054bf6d4372a0d4bc9f
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-12 17:42:32 -0400

    Edits

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,5 +1,6 @@
 // const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
-const QUIZZES_URL = process.env.REACT_APP_QUIZZES_URL;
+//const QUIZZES_URL = process.env.REACT_APP_QUIZZES_URL
+const QUIZZES_URL = 'https://wbdv-sp21-serv-node-becker-sam.herokuapp.com';
 
 export const findQuestionsForQuiz = (qid) =>
   fetch(`${QUIZZES_URL}/${qid}/questions`)
--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,5 +1,6 @@
 // const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
-const QUIZZES_URL = process.env.REACT_APP_QUIZZES_URL;
+// const QUIZZES_URL = process.env.REACT_APP_QUIZZES_URL
+const QUIZZES_URL = 'https://wbdv-sp21-serv-node-becker-sam.herokuapp.com';
 
 
 export const findQuizById = (qid) =>
