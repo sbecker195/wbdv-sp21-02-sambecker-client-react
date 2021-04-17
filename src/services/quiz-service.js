4f5f464e12f17adfa4751ebf9c47f9e73d2af04f
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-17 19:12:32 -0400

    Quiz service

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,7 +1,6 @@
 // const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
-const QUIZZES_URL = process.env.REACT_APP_QUIZZES_URL
 //const QUIZZES_URL = 'https://wbdv-sp21-serv-node-becker-sam.herokuapp.com';
-
+const QUIZZES_URL = process.env.REACT_APP_QUIZZES_URL
 
 export const findQuizById = (qid) =>
   fetch(`${QUIZZES_URL}/${qid}`)
@@ -11,9 +10,21 @@ export const findAllQuizzes = () =>
   fetch(QUIZZES_URL)
     .then(response => response.json())
 
+export const submitQuiz = (qid, questions) =>
+    fetch(`${QUIZZES_URL}/${qid}/attempts`, {
+      method: 'POST',
+      body: JSON.stringify(questions),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json())
+
+export const findQuizAttempts = (qid) =>
+    fetch(`${QUIZZES_URL}/${qid}/attempts`)
+        .then(response => response.json())
 
 const api = {
-  findQuizById, findAllQuizzes
+  findQuizById, findAllQuizzes, submitQuiz, findQuizAttempts
 }
 
 export default api
